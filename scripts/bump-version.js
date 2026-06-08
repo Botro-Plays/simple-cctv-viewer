c842e902804a18a06e2b7c0a487d5946a42958fe
Botro <botro@randiris.com>
2026-06-08 10:09:55 +0800

    build: remove version from output filenames, add auto-bump, add pre-push version tag hook

---
+++ scripts/bump-version.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const pkgPath = path.join(__dirname, '..', 'package.json');
+const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
+
+const [major, minor, patch] = pkg.version.split('.').map(Number);
+const newVersion = `${major}.${minor}.${patch + 1}`;
+pkg.version = newVersion;
+
+fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
+console.log(`Bumped version to ${newVersion}`);
